a2d6c97bc07729a045e1edcc78a98803ab2229f3
peiyu00 <120556342+peiyu00@users.noreply.github.com>
2024-12-28 12:18:36 +0800

    feat(kan-92): update e-card

--- components/eCard/ECardDisplay.jsx
+++ components/eCard/ECardDisplay.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import { useEffect, useState, React } from "react";
 import Image from "next/image";
 import cardImage from "/public/images/Picklesquad_image/vip_premium_card.png";
 import { useSelector } from "react-redux";
 
 const ECardDisplay = () => {
+  const [membership, setMembership] = useState(null);
+  const [error, setError] = useState(null);
   const currentUser = useSelector(state => state.user);
+  const accessToken = useSelector((state) => state.accessToken);
+  const memberId = useSelector((state) => state.user?.user?.membership || null);
+
+  useEffect(() => {
+    if (!memberId) return; 
+ 
+    fetch(`/api/eCard?id=${memberId}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`, 
+      },
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch membership details");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Format expiryDate to YYYY-MM-DD
+      const formattedData = {
+        ...data,
+        expiryDate: new Intl.DateTimeFormat("en-CA", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        }).format(new Date(data.expiryDate)),
+      };
+      setMembership(formattedData);
+    })
+    .catch((error) => {
+      console.error("Error fetching membership details:", error);
+      setError("Failed to load membership details.");
+    });
+  }, [memberId]); 
+  
   
   return (
     <div className="ecard-display">
       <h2 className="ecard-title">Your VIP Membership Card</h2>
       <div className="ecard-container">
+      {error && <p className="error-message">{error}</p>}
         <Image
           src={cardImage}
           alt="VIP Premium Card"
@@ -18,8 +57,8 @@ const ECardDisplay = () => {
         />
         <div className="ecard-overlay">
           <p className="ecard-name">{currentUser.user.name}</p>
-          <p className="ecard-member-no">{currentUser.user._id}</p> 
-          <p className="ecard-valid-thru">12/25</p> 
+          <p className="ecard-member-no">{membership?.membershipId}</p> 
+          <p className="ecard-valid-thru">{membership?.expiryDate}</p> 
         </div>
       </div>
     </div>
