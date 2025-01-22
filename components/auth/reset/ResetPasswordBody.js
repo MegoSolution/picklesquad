a83eea00fed4f382ae9b3ffcc664efd21fddb86e
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2025-01-22 16:38:04 +0800

    feat(main): fix build

--- components/auth/reset/ResetPasswordBody.js
+++ components/auth/reset/ResetPasswordBody.js
@@ -19,7 +19,9 @@ const ResetPassword = () => {
     try {
       console.log(token);
       console.log(password);
-      await axios.post(`${BASE_URL}/auth/reset-password?token=${token}`, { password: password });
+      await axios.post(`${BASE_URL}/auth/reset-password?token=${token}`, {
+        password: password,
+      });
       toast.success('Password has been reset successfully.');
       router.push('/sign-in');
     } catch (error) {
@@ -29,46 +31,52 @@ const ResetPassword = () => {
 
   return (
     <>
-    <section className="section section--space-bottom authentication auth-page authentication--alt wow fadeInUp" data-wow-duration="0.4s">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-lg-8 col-xxl-6">
-            <div className="authentication__wrapper">
-              <h4 className='sign-in-header'>Reset Password</h4>
-              <form onSubmit={handleResetPassword}>
-                <div className="input-single">
-                  <label htmlFor="newPassword">Enter New Password</label>
-                  <input
-                    type="password"
-                    id="newPassword"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter new password"
-                    required />
-                </div>
-                <div className="input-single">
-                  <label htmlFor="confirmPassword">Confirm New Password</label>
-                  <input
-                    type="password"
-                    id="confirmPassword"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    placeholder="Confirm new password"
-                    required />
-                </div>
-                <div className="section__cta text-start">
-                  <button type="submit" className="sign-up-button">
-                    Reset Password
-                  </button>
-                </div>
-                {message && <p>{message}</p>}
-              </form>
+      <section
+        className="section section--space-bottom authentication auth-page authentication--alt wow fadeInUp"
+        data-wow-duration="0.4s"
+      >
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-xxl-6">
+              <div className="authentication__wrapper">
+                <h4 className="sign-in-header">Reset Password</h4>
+                <form onSubmit={handleResetPassword}>
+                  <div className="input-single">
+                    <label htmlFor="newPassword">Enter New Password</label>
+                    <input
+                      type="password"
+                      id="newPassword"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Enter new password"
+                      required
+                    />
+                  </div>
+                  <div className="input-single">
+                    <label htmlFor="confirmPassword">
+                      Confirm New Password
+                    </label>
+                    <input
+                      type="password"
+                      id="confirmPassword"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      placeholder="Confirm new password"
+                      required
+                    />
+                  </div>
+                  <div className="section__cta text-start">
+                    <button type="submit" className="sign-up-button">
+                      Reset Password
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <Toaster />
+      </section>
+      <Toaster />
     </>
   );
 };
