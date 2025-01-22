a5d96dc83b21a7b8ee8fe1038df101d6d1d01a4a
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2025-01-22 17:00:43 +0800

    feat(main): fix checksum

--- utils/checksum.js
+++ utils/checksum.js
@@ -16,14 +16,23 @@ function calculateChecksum({ secretKey, detail, amount, orderId }) {
 function calculateChecksumRecurring({ secretKey, recurringId, orderId }) {
   console.log('DEBUG', secretKey, recurringId, orderId);
   // Concatenate the string in the required format
-  const str = `${secretKey}${recurringId}${orderId}`;
+  // const str = `${secretKey}${recurringId}${orderId}`;
 
-  // Generate HMAC-SHA256 hash
-  const sha256Hash = CryptoJS.HmacSHA256(str, secretKey).toString(
-    CryptoJS.enc.Hex
-  );
+  // // Generate HMAC-SHA256 hash
+  // const sha256Hash = CryptoJS.HmacSHA256(str, secretKey).toString(
+  //   CryptoJS.enc.Hex
+  // );
 
-  return sha256Hash;
+  // return sha256Hash;
+
+  const hash = crypto
+    .createHash('sha256')
+    .update(secretKey + recurringId + orderId)
+    .digest('hex');
+
+  console.log('DEBUG', hash);
+
+  return hash;
 }
 
 export { calculateChecksum, calculateChecksumRecurring };
